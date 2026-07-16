febbc8f868c14808b92872d84e68d847cedadeac
Dogan Alici <doganalici2144@gmail.com>
2026-07-16 12:38:40 +0300

    Add project files.

---
+++ GarageFlow/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GarageFlow.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
