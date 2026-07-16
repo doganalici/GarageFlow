aff94f2ac9642696ac414e04ff278ea6c84c93f0
Dogan Alici <doganalici2144@gmail.com>
2026-07-16 16:14:34 +0300

    Vehicle entity oluşturuldu ve temel alanlar eklendi

---
+++ GarageFlow/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace GarageFlow.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
