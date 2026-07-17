9bf15a27da659a89366cda19c7ea359756b79387
Dogan Alici <doganalici2144@gmail.com>
2026-07-17 16:23:16 +0300

    VehicleCreateViewModel oluşturuldu ve araç ekleme alanları tanımlandı

---
+++ GarageFlow/ViewModels/VehicleCreateViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace GarageFlow.ViewModels
+{
+    public class VehicleCreateViewModel
+    {
+        public string Plate { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string TransmissionType { get; set; }
+        public string EngineType { get; set; }
+        public string FuelType { get; set; }
+        public int Kilometer { get; set; }
+        public int ModelYear { get; set; }
+        public string ChassisNumber { get; set; }
+    }
+}
