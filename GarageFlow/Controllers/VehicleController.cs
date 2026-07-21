1fda6f4d018a941f031e290f3f32aa27b2b9ac55
Dogan Alici <doganalici2144@gmail.com>
2026-07-21 14:44:03 +0300

    Araç ekleme sayfası oluşturuldu ve ViewModel ile bağlandı

---
+++ GarageFlow/Controllers/VehicleController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GarageFlow.Controllers
+{
+    public class VehicleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
